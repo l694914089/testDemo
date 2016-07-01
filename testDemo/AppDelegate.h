89f79a1ee432d6e137143b6be54d7645ba805613
hao <mac_leo@mac-mrli.local>
2016-07-01 10:53:24 +0800

    Initial Commit

---
+++ testDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testDemo
+//
+//  Created by mac_leo on 16/7/1.
+//  Copyright © 2016年 三秒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testDemo
+//
+//  Created by mac_leo on 16/7/1.
+//  Copyright © 2016年 三秒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
